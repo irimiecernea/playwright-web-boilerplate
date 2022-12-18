1b838ad7c4ad126b92d219893eda03850c05140e
Irimie Cernea <irimie.cernea@gmail.com>
2022-12-18 15:05:25 +0200

    improved slider drag function

--- tests/e2e/slider.spec.ts
+++ tests/e2e/slider.spec.ts
@@ -1,10 +1,11 @@
-import {test, expect} from "../../fixtures/page-fixture";
+import { test, expect } from '../../fixtures/page-fixture';
 
-test("Slider Demo", async ({ sliderPO }) => {
-    //go to slider demo page
-    await sliderPO.goto();
-    expect(sliderPO.title).toHaveText(/SIP Calculator/)
+test('Slider Demo', async ({ sliderPO }) => {
+  //go to slider demo page
+  await sliderPO.goto();
+  expect(sliderPO.title).toHaveText(/SIP Calculator/);
 
-    //move the slider in order to set a desired value
-   await  sliderPO.changeSliderValueTo('82500')
+  //move the slider in order to set a desired value
+  await sliderPO.changeSliderValueTo(27_500);
+  await sliderPO.changeSliderValueTo(11_500);
 });
--- tests/page-objects/slider.ts
+++ tests/page-objects/slider.ts
@@ -1,43 +1,58 @@
-import { Locator, Page } from "@playwright/test";
+import { Locator, Page } from '@playwright/test';
 
 export class SliderPO {
   readonly page: Page;
   readonly title: Locator;
-  readonly slider: Locator;
+  readonly sliderHandle: Locator;
   readonly sliderValue: Locator;
+  readonly minSliderValue: number;
+  readonly maxSliderValue: number;
 
   constructor(page: Page) {
     this.page = page;
     this.title = page.locator("'SIP Calculator'").nth(0);
-    this.slider = page.locator("(//div[@role='slider'])[1]");
-    this.sliderValue = page.locator("#MONTHLY_INVESTMENT");
+    this.sliderHandle = page.locator("(//div[@role='slider'])[1]");
+    this.sliderValue = page.locator('#MONTHLY_INVESTMENT');
+    this.minSliderValue = 500;
+    this.maxSliderValue = 100_000;
   }
 
   async goto() {
-    await this.page.goto("https://groww.in/calculators/sip-calculator");
+    await this.page.goto('https://groww.in/calculators/sip-calculator');
   }
 
-  async changeSliderValueTo(amount: string) {
-    await this.slider.waitFor({state: "visible"});
+  async changeSliderValueTo(amount: number) {
+    if (amount < this.minSliderValue || amount > this.maxSliderValue) {
+      throw new Error(
+        `Amount must be no less than ${this.minSliderValue} and no higher than ${this.maxSliderValue}`
+      );
+    }
+
+    await this.sliderHandle.waitFor({ state: 'visible' });
     let isCompleted = false;
-    if (this.slider) {
-      while (!isCompleted) {
-        let srcBound = await this.slider.boundingBox();
-        if (srcBound) {
-          await this.page.mouse.move(
-            srcBound.x + srcBound.width / 2,
-            srcBound.y + srcBound.height / 2
-          );
-          await this.page.mouse.down();
+    while (!isCompleted) {
+      let srcBound = await this.sliderHandle.boundingBox();
+      if (srcBound) {
+        await this.page.mouse.move(
+          srcBound.x + srcBound.width / 2,
+          srcBound.y + srcBound.height / 2
+        );
+        await this.page.mouse.down();
+        if (amount > 25_000) {
           await this.page.mouse.move(
             srcBound.x + 15,
             srcBound.y + srcBound.height / 2
           );
-          await this.page.mouse.up();
-          let text = await this.sliderValue.inputValue();
-          if (text == amount) {
-            isCompleted = true;
-          }
+        } else {
+          await this.page.mouse.move(
+            srcBound.x - 15,
+            srcBound.y + srcBound.height / 2
+          );
+        }
+        await this.page.mouse.up();
+        let text = await this.sliderValue.inputValue();
+        if (text == amount.toString()) {
+          isCompleted = true;
         }
       }
     }
